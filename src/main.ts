c806ae940f5cd2554ff902d0aa502f935f5b7d17
Fernando Zanchetta <fernandozanca94@gmail.com>
2018-10-16 22:38:24 -0300

    started meat-app following udemy course to practice dev with angular

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import './polyfills.ts';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
-import { AppModule } from './app/';
+import { environment } from 'environments/environment';
+import { AppModule } from 'app';
 
 if (environment.production) {
   enableProdMode();
