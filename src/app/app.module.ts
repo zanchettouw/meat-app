6ce60426028643438b04af6af9db46003bbfc1ea
Fernando Zanchetta <fernandozanca94@gmail.com>
2018-11-25 22:25:00 -0200

    learned how use lazy-loading, shared module and preloading function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 
 import { ROUTES } from './app.routes'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
-import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { RatingComponent } from './shared/rating/rating.component';
+
+import { SharedModule } from './shared/shared.module'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
@@ -41,28 +31,16 @@ import { RatingComponent } from './shared/rating/rating.component';
     MenuComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    InputComponent,
-    RadioComponent,
-    OrderItemsComponent,
-    DeliveryCostsComponent,
     OrderSummaryComponent,
-    RatingComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(ROUTES)
+    SharedModule.forRoot(),
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
   ],
-  exports: [
-    ReactiveFormsModule
-  ],
+  exports: [],
   providers: [
-    RestaurantsService,
-    ShoppingCartService,
-    OrderService,
     { provide: LOCALE_ID, useValue: 'pt-BR' }
   ],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,24 @@
-import {Routes} from '@angular/router'
+import { Routes } from '@angular/router'
 
-import {HomeComponent} from './home/home.component'
-import {AboutComponent} from './about/about.component'
-import {RestaurantsComponent} from './restaurants/restaurants.component'
+import { HomeComponent } from './home/home.component'
+import { RestaurantsComponent } from './restaurants/restaurants.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { OrderComponent } from './order/order.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 export const ROUTES: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
-    {path: 'order', component: OrderComponent},
-    {path: 'order-summary', component: OrderSummaryComponent},
-    {path: 'restaurants', component: RestaurantsComponent},
-    {path: 'restaurants/:id', component: RestaurantDetailComponent,
+    { path: '', component: HomeComponent },
+    { path: 'about', loadChildren: './about/about.module#AboutModule' },
+    { path: 'order', loadChildren: './order/order.module#OrderModule' },
+    { path: 'order-summary', component: OrderSummaryComponent },
+    { path: 'restaurants', component: RestaurantsComponent },
+    {
+        path: 'restaurants/:id', component: RestaurantDetailComponent,
         children: [
-            {path: '', redirectTo: 'menu', pathMatch: 'full'},
-            {path: 'menu', component: MenuComponent},
-            {path: 'reviews', component: ReviewsComponent}
-    ]}
+            { path: '', redirectTo: 'menu', pathMatch: 'full' },
+            { path: 'menu', component: MenuComponent },
+            { path: 'reviews', component: ReviewsComponent }
+        ]
+    }
 ]
